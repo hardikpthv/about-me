1224bfbf693f9cb5753c442ab45f17ac2e1139f1
Hardik Pithva <hardikpthv@gmail.com>
2018-09-13 20:51:14 +0530

    fix: onlineedu url

--- hardikpthv/src/utils/Url.js
+++ hardikpthv/src/utils/Url.js
@@ -8,5 +8,5 @@ export const URL = {
   medium: `https://HANDLEdium.com/@${HANDLE}`,
   mediumResource: "https://cdn-images-1.medium.com/max/1600",
   mediumDefaultImage: `https://cdn-images-1.medium.com/max/1600/1*I0E7U5xI-4UvnkExSGKp_w.png`,
-  onlineEdu: 'http://onlineedu.in/tutorials"'
+  onlineEdu: "http://onlineedu.in/tutorials"
 };
